926537091550aa0d19b275ad46f8e6b3947d6518
jose-fernandez <jose.fernandez91@gmail.com>
2017-02-17 08:33:51 +0100

    chat nodeJs, writting user bug

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var cont=0;
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+	cont++;
+  io.sockets.emit("new user", "Un nuevo usuario se ha conectado", cont);
+
+  socket.on('chat message', function(msg, nick){
+    io.sockets.emit('chat message', msg, nick);
+  });
+
+  socket.on("disconnect", function(){
+  	cont--;
+  	io.sockets.emit("user out", "Un usuario se ha desconectado", cont);
+  });
+  socket.on("writing", function(nick){
+  	io.sockets.emit("user writing", `${nick} is writing...`);
+  })
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
