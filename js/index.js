ca54405a7e92376c0c423a6fe36544207f143517
jose-fernandez <jose.fernandez91@gmail.com>
2017-02-19 17:58:35 +0100

    bugs

--- js/index.js
+++ js/index.js
@@ -1,17 +1,11 @@
-var app = require('express')();
-
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var cont=0;
 
 
-http.listen(process.env.PORT || "3000", function(){
-  console.log('listening on *:3000');
-});
-
-app.get('/', function (req, res) {
-    res.sendfile('public/index.html');
-});
+app.use(express.static('public'));
 
 io.on('connection', function(socket){
 	cont++;
@@ -30,3 +24,6 @@ io.on('connection', function(socket){
   });
 });
 
+http.listen(process.env.PORT || "3000", function(){
+  console.log('listening on *:3000');
+});
